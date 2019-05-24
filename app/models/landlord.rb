d8096ed5654c3055c019738367f9aab6882ffc9c
wolf-hub <freelance@web-wolf.xyz>
2019-05-25 00:10:03 +0300

    250519

---
+++ app/models/landlord.rb
@@ -0,0 +1,3 @@
+class Landlord < ApplicationRecord
+  belongs_to :user
+end
---
+++ db/migrate/20190524190112_create_landlords.rb
@@ -0,0 +1,12 @@
+class CreateLandlords < ActiveRecord::Migration[5.2]
+  def change
+    create_table :landlords do |t|
+      t.string :first_name
+      t.string :last_name
+      t.text :information
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
