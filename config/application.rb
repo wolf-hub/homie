d7b0af14cd3b3c80fba54bda02ee2b3b58d9c449
wolf-hub <freelance@web-wolf.xyz>
2019-07-09 17:45:43 +0300

    pipeline

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Homie
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.initialize_on_precompile = false
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
     # Settings in config/environments/* take precedence over those specified here.
