ecf77bef76c2d6c9b8be66188653ab12af76ada1
wolf-hub <freelance@web-wolf.xyz>
2019-07-12 22:48:48 +0300

    congrat and styles fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
-  config.action_cable.url = "wss://homie-seedx.herokuapp.com/cable"
+  config.action_cable.url = "wss://142.93.86.230/cable"
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
