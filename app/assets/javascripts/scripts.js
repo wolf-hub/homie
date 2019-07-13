e31ddaa3a81d644bca397c0082aac673de4a2c49
Sandeep <49954454+sandeep872@users.noreply.github.com>
2019-07-14 01:33:46 +0530

    Update scripts.js

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -563,5 +563,13 @@ $(document).ready(function() {
     });
 });
 
-
+$(document).ready(function() {
+jQuery('option').mousedown(function(e) {
+    e.preventDefault();
+    jQuery(this).toggleClass('selected');
+  
+    jQuery(this).prop('selected', !jQuery(this).prop('selected'));
+    return false;
+});
+});
 
