b841a5a8942ca37eb84ea370006eb9c27cf4b456
wolf-hub <freelance@web-wolf.xyz>
2019-07-17 20:40:45 +0300

    domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,9 +42,9 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
-  config.action_cable.url = '/cable'
+  config.action_cable.url = 'wss://homie.seedx.us/cable'
   # config.action_cable.allowed_request_origins = ['http://142.93.86.230','142.93.86.230']
-  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
+  config.action_cable.allowed_request_origins = ['http://homie.seedx.us']
   config.action_cable.disable_request_forgery_protection = true
 
   # Mount Action Cable outside main process or domain
