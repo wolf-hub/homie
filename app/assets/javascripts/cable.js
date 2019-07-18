8294d14ed386754060b099886e807cfb89bbb1cd
Muhammad Ahmed <ahmed.hanjra96@gmail.com>
2019-07-18 09:12:59 +0500

    web_socket problem

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -8,6 +8,6 @@
 (function() {
   this.App || (this.App = {});
 
-  App.cable = ActionCable.createConsumer("ws://homie.seedx.us/cable");
+  App.cable = ActionCable.createConsumer("/cable");
 
 }).call(this);
