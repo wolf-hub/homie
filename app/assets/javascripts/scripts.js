f726f44bab48b63bde710a82c9886672541a0f74
Muhammad Ahmed <ahmed.hanjra96@gmail.com>
2019-07-18 13:43:43 +0500

    assets precompile

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -479,11 +479,11 @@ jQuery('.visible-pw').click(function() {
         {
             $input.attr("type","text");
             $(this).removeClass("not-visible-pw");
-            $($(this).children('img')[0]).attr("src","assets/visible.png");
+            $($(this).children('img')[0]).attr("src","/assets/visible.png");
         }else{
             $(this).addClass("not-visible-pw");
             $input.attr("type","password");
-            $($(this).children('img')[0]).attr("src","assets/not-visible.png");
+            $($(this).children('img')[0]).attr("src","/assets/not-visible.png");
         }
     // }
 });
