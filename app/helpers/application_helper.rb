c23e6bcd8c86f5406fbf61d0d567a129d2924678
wolf-hub <freelance@web-wolf.xyz>
2019-07-26 19:50:14 +0300

    leftright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,9 +25,9 @@ module ApplicationHelper
 	end	
 
 	def pluralize_without_count(count, noun, text = nil)
-	  if count != 0
+	  
 	    count == 1 ? "#{noun}#{text}" : "#{noun.pluralize}#{text}"
-	  end
+	  
 	end
 
 	def showavatar(user)
