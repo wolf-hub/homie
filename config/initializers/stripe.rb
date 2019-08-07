9d26e01a1c5b098d49e43910da9aad0ff46d8b86
wolf-hub <47275092+wolf-hub@users.noreply.github.com>
2019-08-07 23:20:07 +0300

    Delete stripe.rb

--- config/initializers/stripe.rb
+++
@@ -1,5 +0,0 @@
-Rails.configuration.stripe = {
-  :publishable_key => 'pk_live_mPLykOA8BXTXfzmmm0ozhmZ5',
-  :secret_key => 'sk_live_xCrdypmZKlHzBFdZCfRNOP8M00qzCKFz6g'
-}
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
