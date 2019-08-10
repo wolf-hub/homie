2db05fbace9dbcc106bc5a3a07f685a63e0e3840
wolf-hub <freelance@web-wolf.xyz>
2019-08-10 14:02:13 +0300

    match3

--- app/jobs/matching_job.rb
+++ app/jobs/matching_job.rb
@@ -4,7 +4,7 @@ class MatchingJob < ApplicationJob
   def perform(*args)
     # Do something later
     @properties = Property.where(home_type: @request.home_type, minimum_lease: @request.duration, city: @request.city).where("address like ? and price <= ? and price >= ? ","%#{@request.address}%",@request.max_budget,@request.min_budget)
-    @property.each do |property|
+    @properties.each do |property|
     	PropertyMailer.matching_property_admin_email(property).deliver_later
     end
   end
