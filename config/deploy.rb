e9f56b2f5df36723d5cdfceec8774282d78be9eb
wolf-hub <freelance@web-wolf.xyz>
2019-08-10 20:07:03 +0300

    matchworking6

--- config/deploy.rb
+++ config/deploy.rb
@@ -59,21 +59,3 @@ end
 after 'deploy:starting', 'sidekiq:quiet'
 after 'deploy:reverted', 'sidekiq:restart'
 after 'deploy:published', 'sidekiq:restart'
-
-# If you wish to use Inspeqtor to monitor Sidekiq
-# https://github.com/mperham/inspeqtor/wiki/Deployments
-namespace :inspeqtor do
-  task :start do
-    on roles(:app) do
-      execute :inspeqtorctl, :start, :deploy
-    end
-  end
-  task :finish do
-    on roles(:app) do
-      execute :inspeqtorctl, :finish, :deploy
-    end
-  end
-end
-
-before 'deploy:starting', 'inspeqtor:start'
-after 'deploy:finished', 'inspeqtor:finish'
