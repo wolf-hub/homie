c1e897d3b7d45943e19c4028a386b25eae9cf449
wolf-hub <freelance@web-wolf.xyz>
2019-08-11 11:30:48 +0300

    skq3

--- app/jobs/matching_job.rb
+++ app/jobs/matching_job.rb
@@ -5,7 +5,7 @@ class MatchingJob < ApplicationJob
     # Do something later
     @districts = requesto.address.gsub(/(\[\"|\"\])/, '').split('", "')
     
-    @properties = Property.where("home_type = ? and minimum_lease = ? and city = ? and address = ? and price <= ? and price >= ? ",requesto.home_type,requesto.duration,requesto.city,@districts,requesto.max_budget,requesto.min_budget)
+    @properties = Property.where("home_type = ? and minimum_lease = ? and city = ? and address IN (?) and price <= ? and price >= ? ",requesto.home_type,requesto.duration,requesto.city,@districts,requesto.max_budget,requesto.min_budget)
     @properties.each do |property|
     	PropertyMailer.matching_property_email(property).deliver_later
     end
