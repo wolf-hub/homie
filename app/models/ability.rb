2a449486a047f3938bcf97a74ce7fd9365c6a521
wolf-hub <freelance@web-wolf.xyz>
2019-09-24 13:26:04 +0300

    can

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -67,6 +67,10 @@ class Ability
     can :destroy, Tenant do |tenant|
         tenant.user == user
     end
+
+    if user.admin?
+        can :manage, :all
+    end    
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
