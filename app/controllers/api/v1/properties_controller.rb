6afebaa8265d16487f3b53d5a29d6572365a1119
wolf-hub <freelance@web-wolf.xyz>
2019-10-01 14:47:45 +0300

    sta3

---
+++ app/controllers/api/v1/properties_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::PropertiesController < ApplicationController
+	acts_as_token_authentication_handler_for User
+	respond_to :json
+
+	def index
+		properties = Property.all
+		respond_with properties
+	end
+end	
