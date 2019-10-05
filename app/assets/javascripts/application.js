8ffbaa002f975e7ab8eca258ff7c5d074e52a5de
wolf-hub <freelance@web-wolf.xyz>
2019-10-05 18:40:16 +0300

    assa5

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require_tree .
 //= require jquery
 //= require rails-ujs
 //= require activestorage
@@ -24,3 +23,4 @@
 
 //= require activestorage
 
+//= require_tree .
