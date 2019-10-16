5cac1bfb78e3ecf4f62e8657711fb8d838120897
wolf-hub <freelance@web-wolf.xyz>
2019-10-16 13:24:54 +0300

    aa5

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :role, presence: true       
   enum role: {tenant: 0, landlord: 1}
-  has_one :landlord
-  has_one :tenant
-  has_many :properties  
-  has_many :requests 
+  has_one :landlord, dependent: :destroy
+  has_one :tenant, dependent: :destroy
+  has_many :properties, dependent: :destroy 
+  has_many :requests, dependent: :destroy
   has_many :purchases
   has_many :notifications
   has_many :points
