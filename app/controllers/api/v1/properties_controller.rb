43bad9b9beb63a340665c726d1f2be5c5b41d98e
wolf-hub <freelance@web-wolf.xyz>
2019-10-21 13:38:25 +0300

    sta6

--- app/controllers/api/v1/properties_controller.rb
+++ app/controllers/api/v1/properties_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::PropertiesController < ApplicationController
 	acts_as_token_authentication_handler_for User, fallback: :exception
 	before_action :authenticate_user!
+	protect_from_forgery with: :null_session
 	
 	respond_to :json
 
