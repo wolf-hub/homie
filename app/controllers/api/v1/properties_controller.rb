0a744273a5a053fe813a32a30f2d9ee04eb63cb0
wolf-hub <freelance@web-wolf.xyz>
2019-10-21 20:55:44 +0300

    pr5

--- app/controllers/api/v1/properties_controller.rb
+++ app/controllers/api/v1/properties_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::PropertiesController < ApplicationController
 	
-	acts_as_token_authentication_handler_for User, fallback: :devise
+	acts_as_token_authentication_handler_for User, fallback: :none
 	
 	respond_to :json
 
