453a22be4b7c6a00528420973ca0b7bed470a0ac
wolf-hub <freelance@web-wolf.xyz>
2019-10-22 11:46:28 +0300

    phm2

--- app/controllers/api/v1/properties_controller.rb
+++ app/controllers/api/v1/properties_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::PropertiesController < ApplicationController
 	
 	acts_as_token_authentication_handler_for User, fallback: :none
 	protect_from_forgery with: :null_session
+	before_action :authenticate_with_token!
 	respond_to :json
 
 	def index
