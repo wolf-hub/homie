a8049d7fc72e4fd880de83a59aefddca0865039a
wolf-hub <freelance@web-wolf.xyz>
2019-10-23 14:39:39 +0300

    pr4

--- app/controllers/api/v1/properties_controller.rb
+++ app/controllers/api/v1/properties_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::PropertiesController < ApplicationController
 
 	def index
 		@properties = Property.all
-		@improp = @properties.map { |property| property[:images] = property.images) }
+		@improp = @properties.map { |property| property.merge(image: property.images[0].url) }
 		respond_with @improp
 	end
 end	
