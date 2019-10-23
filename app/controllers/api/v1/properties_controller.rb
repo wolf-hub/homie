b20288a48b9398949216f8a0f413fd53d292136f
wolf-hub <freelance@web-wolf.xyz>
2019-10-23 15:29:10 +0300

    im3

--- app/controllers/api/v1/properties_controller.rb
+++ app/controllers/api/v1/properties_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::PropertiesController < ApplicationController
 	def index
 		@properties = Property.all
 		@properties.map { |property| property_json(property) }
-		respond_with @property
+		respond_with @properties
 	end
 
 	private
