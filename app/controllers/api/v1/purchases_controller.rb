3fce302d16ac7962cee57ebf43adc8e0a5927020
wolf-hub <freelance@web-wolf.xyz>
2019-10-29 14:38:11 +0200

    req8

--- app/controllers/api/v1/purchases_controller.rb
+++ app/controllers/api/v1/purchases_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::UsersController < ApplicationController
+class Api::V1::PurchasesController < ApplicationController
 	
 	acts_as_token_authentication_handler_for User, fallback: :none
 	protect_from_forgery with: :null_session
