a8e4a6ab38e70560576f0a0c03989ffdf497eb5f
wolf-hub <freelance@web-wolf.xyz>
2019-11-24 22:27:37 +0200

    deliver6

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -2,8 +2,8 @@ class MessageMailer < ApplicationMailer
 	default from: 'no-reply@homie.com'
 
 	def new_message_email(message, conversation)
-	    @message = message
-	    @conversation = conversation
+	    @message = Message.find(message);
+	    @conversation = Conversation.find(conversation)
 	    if @message.user == @conversation.sender
 	    	@reciever = @conversation.recepient
 	    else
