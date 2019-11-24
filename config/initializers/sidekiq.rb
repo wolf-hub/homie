a0545db4571e5e78fa359de247eff3b816402086
wolf-hub <freelance@web-wolf.xyz>
2019-11-24 19:23:10 +0200

    deliver4

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379' }
+  config.redis = { url: 'redis://localhost:7372/0' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379' }
+  config.redis = { url: 'redis://localhost:7372/0' }
 end
