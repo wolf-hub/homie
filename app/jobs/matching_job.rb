c0f6f151201075b3d5513abd56a0c5683ab67bc0
wolf-hub <freelance@web-wolf.xyz>
2019-11-26 22:38:29 +0200

    match4

--- app/jobs/matching_job.rb
+++ app/jobs/matching_job.rb
@@ -7,7 +7,7 @@ class MatchingJob < ApplicationJob
     
     @properties = Property.where("home_type = ? and minimum_lease = ? and city = ? and address IN (?) and price <= ? and price >= ? ",requesto.home_type,requesto.duration,requesto.city,@districts,requesto.max_budget,requesto.min_budget)
     @properties.each do |property|
-    	Connection.create(property: property, request: requesto)
+    	Connection.where(:property => property, :request => requesto).first_or_create
     	PropertyMailer.matching_property_email(property, requesto).deliver_later
     end
   end
